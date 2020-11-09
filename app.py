f3d7137f60f8e16c1751189adcf5319dfe03c22f
Kostas Stathoulopoulos <k.stathoylopoylos@gmail.com>
2020-11-09 14:10:47 +0000

    update streamlit app

--- app.py
+++ app.py
@@ -1,37 +1,41 @@
-import streamlit as st
-import pickle
-from sentence_transformers import SentenceTransformer
 import faiss
+import pickle
 import pandas as pd
+import streamlit as st
+from sentence_transformers import SentenceTransformer
 from vector_engine.utils import vector_search
 
 
 @st.cache
-def read_data():
-    return pd.read_csv("s3://vector-search-blog/misinformation_papers.csv")
+def read_data(s3_data="s3://vector-search-blog/misinformation_papers.csv"):
+    """Read the data from S3."""
+    return pd.read_csv(s3_data)
 
 
 @st.cache(allow_output_mutation=True)
 def load_bert_model(name="distilbert-base-nli-stsb-mean-tokens"):
-    # Instantiate the sentence-level DistilBERT
+    """Instantiate a sentence-level DistilBERT model."""
     return SentenceTransformer(name)
 
 
-@st.cache
-def load_faiss_index():
-    with open("models/faiss_index.pickle", "rb") as h:
-        return pickle.load(h)
+@st.cache(allow_output_mutation=True)
+def load_faiss_index(path_to_faiss="models/faiss_index.pickle"):
+    """Load and deserialize the Faiss index."""
+    with open(path_to_faiss, "rb") as h:
+        data = pickle.load(h)
+    return faiss.deserialize_index(data)
 
 
 def main():
+    # Load data and models
     data = read_data()
     model = load_bert_model()
-    faiss_index = faiss.deserialize_index(load_faiss_index())
+    faiss_index = load_faiss_index()
 
     st.title("Vector-based searches with Sentence Transformers and Faiss")
 
     # User search
-    user_input = st.text_area("Search box")
+    user_input = st.text_area("Search box", "covid-19 misinformation and social media")
 
     # Filters
     st.sidebar.markdown("**Filters**")
@@ -49,6 +53,7 @@ def main():
             & (data.year <= filter_year[1])
             & (data.citations >= filter_citations)
         ]
+        # Get individual results
         for id_ in I.flatten().tolist():
             if id_ in set(frame.id):
                 f = frame[(frame.id == id_)]
